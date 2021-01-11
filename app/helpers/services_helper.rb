8626da822df50aaec947fda55d615f547119f97e
agilles1 <adam.g.tpt@gmail.com>
2021-01-11 12:11:06 -0500

    Created ServiceHelperMethods for service view formats

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,23 @@
 module ServicesHelper
+
+    def rehearsal_or_concert(service)
+        if service.concert 
+            "Concert"
+        else
+            "Rehearsal"
+        end
+    end
+
+    def service_duration_format(service)
+        "#{service.start_time.strftime('%I:%M %P')} - #{service.end_time.strftime('%I:%M %P')}"
+    end
+
+    def service_date_format(service)
+        "#{service.start_time.strftime('%A, %B %d, %Y')}"
+    end
+
+    def service_show_format(service)
+        "#{service_date_format(service)} - #{service_duration_format(service)} - #{rehearsal_or_concert(service)}"
+    end
+
 end
