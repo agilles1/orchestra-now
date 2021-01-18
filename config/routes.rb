3c55b6f6241d9c53cbacd9e71c8ac43ba08d34b1
agilles1 <adam.g.tpt@gmail.com>
2021-01-18 14:13:31 -0500

    Update login form to include link to sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
-  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new', as: 'new_user'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
