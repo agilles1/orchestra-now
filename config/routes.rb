1c03a3bb193498416d532560ba15ee11f8758567
agilles1 <adam.g.tpt@gmail.com>
2021-01-23 21:37:10 -0500

    remove unused folders from app direct

--- config/routes.rb
+++ config/routes.rb
@@ -4,27 +4,16 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create', as: 'login_post'
   get '/signup', to: 'users#new', as: 'new_user'
-
-  get '/home', to: 'home#home'
- 
-  resources :services
+  get '/home', to: 'home#home'  
   post '/services/:id/copy', to: 'services#duplicate', as: 'duplicate_service'
   get  '/services/:service_id/work/:id', to: 'service_works#destroy', as: 'delete_service_work'
+  delete '/logout', to: 'sessions#destroy', as: 'log_out'
 
+  resources :services
   resources :works, only: [:index]
-
+  resources :users, only: :create
   resources :composers, only: [:new, :create, :edit, :update]  do 
     resources :works
   end
 
-  
-  resources :users, except: :new
-
-  
-
- 
-  delete '/logout', to: 'sessions#destroy', as: 'log_out'
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
